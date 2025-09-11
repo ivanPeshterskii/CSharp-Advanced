5ba0007f358d6b759bcd5b847beb9398ce45292b
Ivan Pehsterski <peshterskiivan@gmail.com>
2025-09-11 12:16:11 +0300

    Add files via upload

---
+++ Queue/Supermarket/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Supermarket;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Queue<string> queue = new Queue<string>();
+
+        string command = string.Empty;
+        while ((command=Console.ReadLine())!="End")
+        {
+            queue.Enqueue(command);
+
+            if (command == "Paid")
+            {
+                Console.WriteLine(queue.Dequeue());
+                queue.Clear();
+            }
+
+        }
+        Console.WriteLine($"{queue.Count} people remaining.");
+        Console.ReadKey();
+        
+    }
+}
+
---
+++ Queue/TrafficJam/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TrafficJam;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        Queue<string> queue = new Queue<string>();
+        int count = 0;
+
+        string command = "";
+        while ((command = Console.ReadLine())!= "end")
+        {
+            if (command == "green")
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    if (queue.Any())
+                    {
+                        string car = queue.Dequeue();
+                        Console.WriteLine($"{car} passed!");
+                        count++;
+                    }
+                }
+            }
+            else
+            {
+                queue.Enqueue(command);
+            }
+        }
+        Console.WriteLine($"{count} cars passed the crossroads.");
+        Console.ReadKey();
+    }
+}
+

5b4caa35efc2b45296ff53f2f063aa67ae9105e9
Ivan Pehsterski <peshterskiivan@gmail.com>
2025-09-30 22:10:16 +0300

    Add files via upload

---
+++ CustomDoublyLinkedList/CustomDoublyLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,108 @@
+﻿using System;
+namespace CustomDoublyLinkedList
+{
+	public class DoublyLinkedList
+	{
+		private class ListNode
+		{
+			public int Value { get; set; }
+
+			public ListNode NextNode { get; set; }
+
+            public ListNode PreviousNode { get; set; }
+
+			public ListNode(int value)
+			{
+				this.Value = value;
+			}
+        }
+		private ListNode head;
+		private ListNode tail;
+
+		public int Count { get; private set; }
+
+		public void AddFirstElement(int element)
+		{
+			if(this.Count == 0)
+			{
+				this.head = this.tail = new ListNode(element);
+			}
+			else
+			{
+				var newHead = new ListNode(element);
+				newHead.NextNode = this.head;
+				this.head.PreviousNode = newHead;
+				this.head = newHead;
+			}
+			this.Count++;
+		}
+
+		public int RemoveFirst(int element)
+		{
+			if (this.Count == 0)
+			{
+				throw new InvalidOperationException("The list is emoty");
+			}
+
+			var firstElement = this.head.Value;
+			this.head = this.head.NextNode;
+			if (this.head != null)
+			{
+				this.head.PreviousNode = null;
+			}
+			else
+			{
+				this.tail = null;
+			}
+			this.Count--;
+			return firstElement;
+		}
+
+		public int RemoveLast(int element)
+		{
+			if(this.Count == 0)
+			{
+				throw new InvalidOperationException("The list is emoty");
+
+			}
+
+			var lastElement = this.head.Value;
+			this.head = this.head.PreviousNode;
+			if(this.head != null)
+			{
+				this.head.NextNode = null;
+			}
+			else
+			{
+				this.tail = null;
+			}
+			this.Count--;
+			return lastElement;
+		}
+
+		public void ForEach(Action<int> action)
+		{
+			var current = this.head;
+			while (current != null)
+			{
+				action(current.Value);
+				current = current.NextNode;
+			}
+		}
+
+		public int[] ToArray()
+		{
+			int[] array = new int[this.Count];
+
+			int counter = 0;
+			var currentNode = this.head;
+			while (currentNode != null) 
+			{
+				array[counter] = currentNode.Value;
+				currentNode = currentNode.NextNode;
+				counter++;
+			}
+			return array;
+		}
+	}
+}
---
+++ CustomDoublyLinkedList/CustomDoublyLinkedList/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace CustomDoublyLinkedList;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DoublyLinkedList list = new DoublyLinkedList();
+
+        list.AddFirstElement(3);
+        list.AddFirstElement(10);
+        list.AddFirstElement(1);
+
+        list.RemoveFirst(1);
+
+        Console.WriteLine(string.Join(", ", list.ToArray()));
+        Console.ReadKey();
+    }
+}
+
